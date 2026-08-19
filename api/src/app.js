fe0b22f607a9eae9d4e8f12a5f4fde0c0d66aba6
Anorak100 <israelakoteyon@gmail.com>
2026-08-20 00:59:21 +0100

    chore: include frontend url in the allowedOrigins variable of backend to allow communication

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
-  'https://tasknode.vercel.app',
+  'https://task-node-sand.vercel.app',
 ];
 
 app.use(cors({
